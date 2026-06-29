1d1f2d8d162446efb2ab996a9af9f450a9a4ba90
Jamjamfm <121386973+Fmjamjam@users.noreply.github.com>
2026-06-29 13:17:28 +0800

    Implement basic Telegram bot with start command

---
+++ bot.py
@@ -0,0 +1,23 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Bot Online ✅"
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(
+    CommandHandler(
+        "start",
+        start
+    )
+)
+
+print("Bot berjalan...")
+
+app.run_polling()
